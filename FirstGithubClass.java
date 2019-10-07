100f906aac7132fb5e37c82dd26aff44d96f7d4d
sunqiange <56259713+sunqiange@users.noreply.github.com>
2019-10-07 21:47:19 +0800

    优化代码
    一点点修改哦

--- FirstGithubClass.java
+++ FirstGithubClass.java
@@ -2,6 +2,6 @@ package Gihub;
 
 public class FirstGithubClass {
     public static void main(String[] args) {
-        System.out.println("Hello,Github!");
+        System.out.println("Hello,Github!Again!");
     }
 }
